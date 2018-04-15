1f870151da69d42cad0c7ad278d3dc4cc211f85c
DESKTOP-R0BP5KK\xipher-desktop <yukino.konata@gmail.com>
2018-04-16 01:34:33 +0900

    簡易の翻訳つぶやき追加。未検証

--- php/aabot.php
+++ php/aabot.php
@@ -3,10 +3,23 @@
 include __DIR__.'/vendor/autoload.php';
 
 $tokenStr = file_get_contents("../token.json");
-$discordToken = json_decode($tokenStr)->discordTokenPhp;
+$tokens = json_decode($tokenStr);
+$discordToken = $tokens->discordTokenPhp;
+$translationToken = $tokens->discordTokenPhp;
+$projectId = $tokens->pjid;
 
+# setup discord instance
 $discord = new \Discord\Discord([
     'token' => $discordToken, // ←作成したBotのTokenを入力してね
+]);
+
+# setup google translate instance
+use Google\Cloud\Translate\TranslateClient;
+
+$translate = new TranslateClient();
+$translate = new TranslateClient([
+    'projectId' => $projectId,
+    'key' => $translationToken
 ]);
 
 $discord->on('ready', function ($discord) {
@@ -14,6 +27,22 @@ $discord->on('ready', function ($discord) {
 
     // Listen for events here
     $discord->on('message', function ($message) {
+        var_dump({$message->author->username);
+        if ({$message->author->username != "AAdiscobot"){
+            # 言語検出
+            $detectresult = $translate->detectLanguage($massage->content);
+            echo "Is {$result['languageCode']}";
+
+            # 翻訳ターゲット選定 ja -> en, Other -> ja
+            $translateLanguage = 'ja';
+            if ($result['languageCode'] == 'ja'){
+                $translateLanguage = 'en';
+            }
+
+            # 翻訳後の文字列を発言
+            $translatedText = $translate->translate($massage->content);
+            $massage->sendMessage($text);
+        }
         echo "Recieved a message from {$message->author->username}: {$message->content}", PHP_EOL;
     });
 });
